6271e740f11c1ece65bd411db32d1a131619c80a
Jizhou Liao <jizhou.liao@nxp.com>
2014-06-06 14:42:28 -0700

    Memory leak and NULL pointer check fixes
    Change-Id: Icea86e8b263386679dc65da90229dd08aed91074

--- src/nfc/nfc/nfc_ncif.c
+++ src/nfc/nfc/nfc_ncif.c
@@ -16,7 +16,25 @@
  *
  ******************************************************************************/
 
-
+/******************************************************************************
+ *
+ *  The original Work has been changed by NXP Semiconductors.
+ *
+ *  Copyright (C) 2013-2014 NXP Semiconductors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
 /******************************************************************************
  *
  *  This file contains functions that interface with the NFC NCI transport.
@@ -325,7 +343,12 @@ void nfc_ncif_check_cmd_queue (BT_HDR *p_buf)
                     nfc_cb.flags         &= ~NFC_FL_DISCOVER_PENDING;
                     ps                    = (UINT8 *)nfc_cb.p_disc_pending;
                     nci_snd_discover_cmd (*ps, (tNFC_DISCOVER_PARAMS *)(ps + 1));
-                    GKI_freebuf (nfc_cb.p_disc_pending);
+                    if(nfc_cb.p_last_disc)
+                    {
+                        GKI_freebuf (nfc_cb.p_last_disc);
+                        nfc_cb.p_last_disc = NULL;
+                    }
+                    nfc_cb.p_last_disc = nfc_cb.p_disc_pending;
                     nfc_cb.p_disc_pending = NULL;
                 }
             }
