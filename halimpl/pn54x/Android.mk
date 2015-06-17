fecb4c5ec16d8589bebf0f22c41d7c7f62a357c4
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-06-17 09:38:44 +0200

    Rename NXP HAL
    Renamed to nfc_nci to maintain compatibility on existing
    platforms
    
    Change-Id: I4f4e2e2aaf9d7b568f4b94710d7c8751542d3986
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- halimpl/pn54x/Android.mk
+++ halimpl/pn54x/Android.mk
@@ -25,7 +25,7 @@ LOCAL_PRELINK_MODULE := false
 LOCAL_ARM_MODE := arm
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE := nfc_nci.$(TARGET_DEVICE)
+LOCAL_MODULE := nfc_nci.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := $(call all-c-files-under, .)  $(call all-cpp-files-under, .)
 LOCAL_SHARED_LIBRARIES := liblog libcutils libhardware_legacy libdl libhardware
